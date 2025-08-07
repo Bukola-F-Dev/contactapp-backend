5defb7c94397840fd31190a5433b684ebad0fc7c
Florence <florencebukola046@gmail.com>
2025-08-07 14:46:40 +0100

    commit

--- server.js
+++ server.js
@@ -8,22 +8,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-const cors = require('cors');
 app.use(cors());
-
 app.use(express.json());
-
 app.use('/api/contacts', contactRoutes);
-
-
 const PORT = process.env.PORT || 5000;
-
-
 app.get('/', (req, res) => {
   res.send('Welcome to my backend API!');
 });
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
