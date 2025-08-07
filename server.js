0ca1e6e4df395da2cb7c09ddc3357c10dfa635cb
Florence <florencebukola046@gmail.com>
2025-08-07 12:06:08 +0100

    Add root route

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use('/api/contacts', contactRoutes);
 
 
 const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my backend API!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
